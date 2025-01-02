7145323015b7302a272a85c7e1f7d6f0434ff319
Anastasia Voznyak <Anastasiavoznak774@gmail.com>
2025-01-02 20:57:59 +0700

    Add static course card component

---
+++ src/components/shared/course/Course.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react'
+
+import { Typography } from '@/components/ui'
+
+import styles from './Course.module.scss'
+
+interface ICourse {
+	courseName: string
+	author: string
+	cover: string
+	price: number
+}
+
+const Course: FC<ICourse> = ({ courseName, author, cover, price }) => {
+	return (
+		<div className={styles.course}>
+			<img src='cover.svg' alt='cover' className={styles.course__cover} />
+			<div className={styles.course__info}>
+				<Typography variant='text-large'>Beginner’s Guide to Design</Typography>
+				<Typography variant='text-medium'>By Ronald Richards</Typography>
+				<Typography variant='text-medium'>$149.9</Typography>
+			</div>
+		</div>
+	)
+}
+
+export default Course
