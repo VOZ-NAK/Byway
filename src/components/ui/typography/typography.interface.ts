b7218f976d1f4f311367f0a7cba380b4243671d3
Anastasia Voznyak <Anastasiavoznak774@gmail.com>
2025-01-02 19:23:15 +0700

    Add typography and common styles

---
+++ src/components/ui/typography/typography.interface.ts
@@ -0,0 +1,13 @@
+import { ComponentProps } from 'react'
+
+type TVariant =
+	| 'title-small'
+	| 'title-medium '
+	| 'title-large'
+	| 'text-small '
+	| 'text-medium'
+	| 'text-large '
+
+export interface ITypography extends ComponentProps<'p'> {
+	variant: TVariant
+}
