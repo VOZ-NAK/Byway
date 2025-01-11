8e3107661af18648d7820169282f21c325169dc0
Anastasia Voznyak <Anastasiavoznak774@gmail.com>
2025-01-11 18:29:57 +0700

    Add icon font

---
+++ src/components/shared/index.ts
@@ -0,0 +1,4 @@
+export { default as User } from './user/User'
+export { default as Course } from './course/Course'
+export { default as Head } from './head/Head'
+export { default as Swiper } from './swiper/Swiper'
