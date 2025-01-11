6e7cb6991d6d994cdb6e84ea6e2cf595f01a44ef
Anastasia Voznyak <Anastasiavoznak774@gmail.com>
2025-01-11 18:18:17 +0700

    Add render mock data to Category

--- src/components/screens/home/components/category/Category.tsx
+++ src/components/screens/home/components/category/Category.tsx
@@ -2,24 +2,37 @@ import { FC } from 'react'
 
 import { Typography } from '@/components/ui'
 
-import styles from './Category.module.scss'
-
-interface ICategory {
-	icon: string
-	categoryName: string
-	courseCount: number
-}
+import { ICategory } from './category.interface'
+import styles from './category.module.scss'
 
 const Category: FC<ICategory> = ({ icon, categoryName, courseCount }) => {
 	return (
 		<div className={styles.card}>
-			<img src='circle.svg' alt='circle' className={styles.card__icon} />
+			<img
+				src={icon}
+				alt={`${categoryName} icon`}
+				className={styles.card__icon}
+			/>
 			<div className={styles.card__info}>
-				<Typography variant='text-large'>Astrology</Typography>
-				<p className={styles.card__count}>11 Courses</p>
+				<Typography variant='text-large'>{categoryName}</Typography>
+				<Typography variant='text-medium'>{courseCount} Courses</Typography>
 			</div>
 		</div>
 	)
 }
 
 export default Category
+
+// Category component for displaying a category of courses
+//
+// Props:
+// - icon: URL or path to the category icon image
+// - categoryName: the name of the category
+// - courseCount: the number of courses in this category
+//
+// Example usage:
+// <Category
+//   icon="path/to/icon.svg"
+//   categoryName="Astrology"
+//   courseCount={11}
+// />
---
+++ src/components/screens/home/components/category/category.interface.ts
@@ -0,0 +1,5 @@
+export interface ICategory {
+	icon: string
+	categoryName: string
+	courseCount: number
+}
